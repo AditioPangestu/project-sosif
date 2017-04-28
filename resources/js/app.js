aa5c3d665326064805d57e73b69af47bafff3b17
Aditio Pangestu <aditiopangestu@gmail.com>
2017-04-28 18:41:02 +0700

    Membuat fitur tambah dan view to do

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,13 +2,13 @@ var toDoListApp = angular.module('toDoListApp',[]);
 
 toDoListApp.controller('toDoListController',['$scope', function($scope){
     
-    addToDo(){
+    $scope.addToDo = function(){
         $scope.toDoList.push ({
-            judul : $scope.newToDO.judul,
-            deskripsi : $scope.newToDO.deskripsi,
-            tglMulai : "01/04/2017",
-            tglSelesai : "20/04/2017",
-            prioritas : "tinggi",
+            judul : $scope.newToDo.judul,
+            deskripsi : $scope.newToDo.deskripsi,
+            tglMulai : $scope.newToDo.tglMulai,
+            tglSelesai : $scope.newToDo.tglSelesai,
+            prioritas : $scope.newToDo.prioritas,
             sudahDilakukan : false
         });
     }
@@ -17,24 +17,24 @@ toDoListApp.controller('toDoListController',['$scope', function($scope){
         {
             judul : "Membuat PR Sister",
             deskripsi : "Implementasi algoritma rifting",
-            tglMulai : "01/04/2017",
-            tglSelesai : "20/04/2017",
+            tglMulai : new Date(2017,4,1),
+            tglSelesai : new Date(2017,4,19),
             prioritas : "tinggi",
             sudahDilakukan : false
         },
         {
             judul : "Membuat PR PBD",
             deskripsi : "Membuat subsistem android",
-            tglMulai : "19/04/2017",
-            tglSelesai : "26/04/2017",
+            tglMulai : new Date(2017,4,19),
+            tglSelesai : new Date(2017,4,26),
             prioritas : "Sedang",
             sudahDilakukan : false
         },
         {
             judul : "Belajar Angular JS",
             deskripsi : "Buka website dokumentasi angular js",
-            tglMulai : "21/04/2017",
-            tglSelesai : "26/04/2017",
+            tglMulai : new Date(2017,4,23),
+            tglSelesai : new Date(2017,4,26),
             prioritas : "Rendah",
             sudahDilakukan : false
         }
